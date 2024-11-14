9fe509754761aa0797f3cd0dc21079df4fb8734b
amenaaUMCA <amenaa@castrocarazo.ac.cr>
2024-11-13 18:06:14 -0600

    Actulizado
    Cambio de librerias

--- analisis_datos/estadisticas.py
+++ analisis_datos/estadisticas.py
@@ -1,12 +1,1 @@
 # estadisticas.py
-def media(datos):
-    return sum(datos) / len(datos)
-
-def mediana(datos):
-    datos_sorted = sorted(datos)
-    n = len(datos)
-    mid = n // 2
-    if n % 2 == 0:
-        return (datos_sorted[mid - 1] + datos_sorted[mid]) / 2.0
-    else:
-        return datos_sorted[mid]
